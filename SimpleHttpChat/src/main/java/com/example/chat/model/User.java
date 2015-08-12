84292e9a60ecdf50a3df6b61a73fa7bc27681192
dhuy93 <ldhuy@USER-PC>
2015-08-12 18:01:42 +0700

    Config database, add User model.

---
+++ SimpleHttpChat/src/main/java/com/example/chat/model/User.java
@@ -0,0 +1,106 @@
+/**
+ * 
+ */
+package com.example.chat.model;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @author ldhuy
+ *
+ */
+@Document(collection = "users")
+public class User {
+	
+	@Id
+	private ObjectId id;
+	
+	@Indexed(unique=true)
+	private String email;
+	
+	private String firstname;
+	private String lastname;
+	private String password;
+	
+	/**
+	 * 
+	 */
+	public User() {
+	}
+
+	/**
+	 * @return the id
+	 */
+	public ObjectId getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the firstname
+	 */
+	public String getFirstname() {
+		return firstname;
+	}
+
+	/**
+	 * @param firstname the firstname to set
+	 */
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	/**
+	 * @return the lastname
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+
+	/**
+	 * @param lastname the lastname to set
+	 */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
