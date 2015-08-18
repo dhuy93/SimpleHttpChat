60fa5d77cda52d2f224b59ce8c4e2b960977eacb
dhuy93 <ldhuy@USER-PC>
2015-08-18 12:33:01 +0700

    Configure database connection and implement model for users and messages.

--- SimpleHttpChat/src/main/java/com/example/chat/HomeController.java
+++ SimpleHttpChat/src/main/java/com/example/chat/HomeController.java
@@ -6,17 +6,23 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.chat.model.Message;
+import com.example.chat.model.User;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+	@Autowired
+	MongoTemplate mongoTpl;
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -32,6 +38,19 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		User sender = new User();
+		User receiver = new User();
+		sender.setEmail("abc@cba.com");
+		sender.setFirstname("Huy");
+		receiver.setEmail("xyz@xyz.com");
+		receiver.setFirstname("Binh");
+		Message msg = new Message();
+		msg.setSender(sender);
+		msg.setReceiver(receiver);
+		msg.setCreatedTime(new Date(System.currentTimeMillis()));
+		msg.setContent("test 123");
+		mongoTpl.insert(msg);
 		
 		return "home";
 	}
---
+++ SimpleHttpChat/src/main/java/com/example/chat/model/Message.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package com.example.chat.model;
+
+import java.util.Date;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @author ldhuy
+ *
+ */
+@Document(collection = "messages")
+public class Message {
+
+	@Id
+	private ObjectId id;
+	private User sender;
+	private User receiver;
+	private Date createdTime;
+	private String content;
+	
+	public Message() {
+		
+	}
+
+	/**
+	 * @return the id
+	 */
+	public ObjectId getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the sender
+	 */
+	public User getSender() {
+		return sender;
+	}
+
+	/**
+	 * @param sender the sender to set
+	 */
+	public void setSender(User sender) {
+		this.sender = sender;
+	}
+
+	/**
+	 * @return the receiver
+	 */
+	public User getReceiver() {
+		return receiver;
+	}
+
+	/**
+	 * @param receiver the receiver to set
+	 */
+	public void setReceiver(User receiver) {
+		this.receiver = receiver;
+	}
+
+	/**
+	 * @return the createdTime
+	 */
+	public Date getCreatedTime() {
+		return createdTime;
+	}
+
+	/**
+	 * @param createdTime the createdTime to set
+	 */
+	public void setCreatedTime(Date createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * @param content the content to set
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
